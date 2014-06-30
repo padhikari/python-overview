ab64e98ae256cef49588ef776c5917fada9fd07f
pabitraadhikari@gmail.com <pabitraadhikari@gmail.com>
2014-06-29 22:52:00 -0700

    function with defaut argument

---
+++ function2.py
@@ -0,0 +1,8 @@
+def printinfo( name, age = 30 ):
+    print "Name: ", name;
+    print "Age ", age;
+    return;
+
+# call printinfo function
+printinfo( age=50, name="mike" );
+printinfo( name="mike" );
