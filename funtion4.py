1397e1699cad175246b32f276441dfcc79eba07f
pabitraadhikari@gmail.com <pabitraadhikari@gmail.com>
2014-06-29 23:00:04 -0700

    funtion with return value

---
+++ funtion4.py
@@ -0,0 +1,10 @@
+
+def sum( arg1, arg2 ):
+    # Add parameters and return them."
+    total = arg1 + arg2
+    print "Inside the function : ", total
+    return total;
+
+# can call sum function
+total = sum( 10, 20 );
+print "Outside the function : ", total
